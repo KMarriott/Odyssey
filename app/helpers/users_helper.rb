211408c2f9b2742cd9ea2fbc90496fd863539743
luke pate <lukewpate@gmail.com>
2017-04-27 15:34:55 -0400

    air and  eventful api

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def  primary_image(response)
+      response = response["image"] || {"small" => { "url" => "foo.jpg"}}
+  end
+
+  def  primary_time(response)
+      response = response["stop_time"] || "forever"
+  end
+  
+  def  primary_address(response)
+      response = response["venue_address"] || response["city_name"]
+  end
 end
