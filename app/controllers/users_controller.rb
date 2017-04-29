7a0b484873decd3a40dca481bded6e2a39fb44d3
luke pate <lukewpate@gmail.com>
2017-04-29 17:31:51 -0400

    lots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,5 @@ class UsersController < ApplicationController
     response = HTTParty.get('http://api.eventful.com/json/events/search?location=Gainesville,fl&app_key=sZH3jKCFNTDRW7xC')
     @response = JSON.parse(response)
     @response = @response["events"]["event"]
-
   end
-
-
 end
