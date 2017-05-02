3d0a368495393fdd37636e0fb860e6d653f5e236
luke pate <lukewpate@gmail.com>
2017-05-02 18:49:28 -0400

    hide paris

--- app/helpers/oahu_helper.rb
+++ app/helpers/oahu_helper.rb
@@ -1,2 +1,26 @@
 module OahuHelper
+
+  def format_date(arr)
+    date = arr["OutboundLeg"]["DepartureDate"]
+    date_arr = date.split("T")
+    date_split = date_arr[0].split("-")
+    date_combine = "#{date_split[1]}-#{date_split[2]}-#{date_split[0]}"
+    puts date_combine
+    return date_combine
+  end
+  def format_date(arr)
+    time = primary_time(arr)
+    puts time
+    if time == "forever"
+      return "forever"
+    else
+
+    end
+    # date = arr["OutboundLeg"]["DepartureDate"]
+    # date_arr = date.split("T")
+    # date_split = date_arr[0].split("-")
+    # date_combine = "#{date_split[1]}-#{date_split[2]}-#{date_split[0]}"
+    # puts date_combine
+    # return date_combine
+  end
 end
